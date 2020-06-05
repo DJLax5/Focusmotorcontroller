2a2612f2d7a86161f0a53e41b6848b739d545bda
DJLax5 <drheinz@gmx.de>
2020-06-05 12:53:12 +0200

    Initial Commit
    Only ATMEL Studio Project Setup

---
+++ Focusmotorcontroller/main.c
@@ -0,0 +1,18 @@
+/*
+ * Focusmotorcontroller.c
+ *
+ * Created: 05.06.2020 12:50:49
+ * Author : FHoch
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
